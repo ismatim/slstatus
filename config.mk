d9733c5ad29f64d908d190b19a29f8424013b685
Ismael J. Tisminetzky <me@ismaeltisminetzky.xyz>
2021-12-26 10:52:38 -0300

    custom config

--- config.mk
+++ config.mk
@@ -7,15 +7,16 @@ VERSION = 0
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Os
-LDFLAGS  = -L$(X11LIB) -s
+# LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm
+LDFLAGS  = -L$(X11LIB) -lkvm
 LDLIBS   = -lX11
 
 # compiler and linker
